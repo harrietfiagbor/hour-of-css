5029b6d3b323f065b744716fb92b755a594077f3
harrietfiagbor <harrietfiagbor@gmail.com>
2019-01-26 11:48:54 +0000

    Added javascript to index

---
+++ main.js
@@ -0,0 +1,9 @@
+window.onload = function(e) {
+alert("Today is the first day of the rest of my life! ");
+
+var harriet = document.getElementById('developer');
+
+console.log(harriet.innerText);
+
+harriet.innerText="Hannah"
+};
